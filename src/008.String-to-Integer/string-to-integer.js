7f67ca05afed6dc59b3ceec1b2be0113ed0bd5c4
AllenLin <allen0321.lin@ehsn.com.tw>
2020-06-16 11:23:34 +0800

    pass 5 測試

--- src/008.String-to-Integer/string-to-integer.js
+++ src/008.String-to-Integer/string-to-integer.js
@@ -1,3 +1,5 @@
+const INT_MAX = 2147483647;
+const INT_MIN = -2147483648;
 /**
  * @param {string} str
  * @return {number}
@@ -6,6 +8,9 @@ const myAtoi = function (str) {
   const firstStrCode = str.trim().charCodeAt(0);
   if (firstStrCode >= 48 && firstStrCode < 57) {
     const strArr = str.split(" ");
+
+    if (+strArr[0] > INT_MAX) return INT_MAX;
+    if (+strArr[0] < INT_MIN) return INT_MIN;
     return +strArr[0];
   } else if (firstStrCode === 45) {
     return +str;
